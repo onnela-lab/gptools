a2444b2ec1487d621dc70290e2fce113375f1c0f
Till Hoffmann <tillahoffmann@gmail.com>
2022-08-11 13:30:31 -0400

    Add profiling tasks to `dodo.py`.

--- dodo.py
+++ dodo.py
@@ -25,3 +25,19 @@ for path in pathlib.Path("graph_gaussian_process/examples").glob("*/*.ipynb"):
     target = path.with_suffix(".html")
     manager(basename="compile_example", name=path.with_suffix("").name, file_dep=[path],
             targets=[target], actions=[f"jupyter nbconvert --execute --to=html {path}"])
+
+
+# Run different profiling configurations. We expect the centered parametrization to be better for
+# strong data and the non-centered parametrization to be better for weak data.
+profile_configurations = {
+    "centered-weak": ("centered", 1.0),
+    "centered-strong": ("centered", 0.1),
+    "non_centered-weak": ("non_centered", 1.0),
+    "non_centered-strong": ("non_centered", 0.1),
+}
+for name, configuration in profile_configurations.items():
+    target = f"workspace/{name}.pkl"
+    args = ["python", "-m", "graph_gaussian_process.profile", *configuration, target,
+            "--iter_sampling=100"]
+    manager(basename="profile", name=name, actions=[args], targets=[target],
+            file_dep=["graph_gaussian_process/profile/__main__.py"])
