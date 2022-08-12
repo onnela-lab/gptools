fb78209ac7288c6a71e6ecaeae80f2a42ffc6de5
Till Hoffmann <tillahoffmann@gmail.com>
2022-08-12 12:22:10 -0400

    Adapt profiling test to new receptive field.

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -11,10 +11,14 @@ def test_profile(parametrization: typing.Literal["centered", "non_centered"]) ->
     iter_sampling = 2
     chains = 4  # cmdstanpy default.
     num_nodes = 7
+    num_parents = 2
     with tempfile.TemporaryDirectory() as tempdir:
         filename = pathlib.Path(tempdir, "result.pkl")
-        __main__([parametrization, "1.0", str(filename), f"--iter_sampling={iter_sampling}",
-                  f"--num_nodes={num_nodes}", "--show_diagnostics", "--compile=force"])
+        __main__([
+            parametrization, "1.0", str(filename), f"--iter_sampling={iter_sampling}",
+            f"--num_nodes={num_nodes}", "--show_diagnostics", "--compile=force",
+            f"--num_parents={num_parents}",
+        ])
         with filename.open("rb") as fp:
             result = pickle.load(fp)
 
