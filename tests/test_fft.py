Fix scaling and add scale validation.

tests/test_fft.py
@@ -4,14 +4,14 @@ import pytest
 from scipy import stats
 
 
-@pytest.fixture(params=[20, 21])
+@pytest.fixture(params=[20, 21], scope="session")
 def data(request: pytest.FixtureRequest) -> dict:
     n: int = request.param
     x = np.arange(n)
     ys = []
     covs = []
     log_probs = []
-    m = 17
+    m = 1_000
     for _ in range(m):
         kernel = ExpQuadKernel(np.random.gamma(10, 0.1), np.random.gamma(10, 0.1), 1e-2, x.size)
         cov = kernel(x[:, None])
@@ -60,13 +60,22 @@ def test_log_prob_fft_normal(data: dict, method: str, dist: str) -> None:
     assert fftvar.shape == shape
     assert ffts.shape == shape
 
-    # Get the scales.
+    # Evaluate the scales and rescale the Fourier coefficients.
     fft_scale = np.sqrt(fftvar / 2)
     fft_scale[:, 0] *= np.sqrt(2)
 
-    scaled_ffts = ffts / fft_scale
     if n % 2 == 0:
-        scaled_ffts[:, n // 2] /= np.sqrt(2)
+        fft_scale[:, n // 2] *= np.sqrt(2)
+    scaled_ffts = ffts / fft_scale
+
+    np.testing.assert_allclose(scaled_ffts[:, 0].imag, 0, atol=1e-9)
+    if n % 2 == 0:
+        np.testing.assert_allclose(scaled_ffts[:, n // 2].imag, 0, atol=1e-9)
+
+    assert np.abs(np.std(scaled_ffts.real, axis=0) - 1).max() < 0.1
+    # For even `n`, the last Fourier coefficient also has zero imaginary part.
+    idx = n // 2 if n % 2 else n // 2 - 1
+    assert np.abs(np.std(scaled_ffts.imag[:, 1:idx], axis=0) - 1).max() < 0.1
 
     # Scale the fourier transforms and evaluate the likelihood.
     if dist == "norm":
@@ -80,7 +89,7 @@ def test_log_prob_fft_normal(data: dict, method: str, dist: str) -> None:
         log_prob = stats.norm().logpdf(scaled_ffts.real) @ rweight \
             + stats.norm().logpdf(scaled_ffts.imag) @ iweight \
             - np.log(fft_scale) @ (iweight + rweight) \
-            + np.log(2) / 2 * (1 - n) + n * np.log(n) / 2
+            - np.log(2) * ((n - 1) // 2) + n * np.log(n) / 2
     elif dist == "chi2":
         pytest.skip()
     else: