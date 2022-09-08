dfae2b2f00d5edf8e6466becf63255cc67821c00
Till Hoffmann <tillahoffmann@gmail.com>
2022-09-07 22:40:33 -0400

    Add periodicity test and kernel base class.

--- gptools-util/gptools/util/kernels.py
+++ gptools-util/gptools/util/kernels.py
@@ -4,10 +4,15 @@ from . import ArrayOrTensor, ArrayOrTensorDispatch
 dispatch = ArrayOrTensorDispatch()
 
 
-def evaluate_squared_distance(x: ArrayOrTensor, y: ArrayOrTensor = None,
-                              period: ArrayOrTensor = None) -> ArrayOrTensor:
+def evaluate_residuals(x: ArrayOrTensor, y: ArrayOrTensor = None, period: ArrayOrTensor = None) \
+        -> ArrayOrTensor:
     """
-    Evaluate the squared distance between points respecting periodic boundary conditions.
+    Evaluate the residuals between points respecting periodic boundary conditions.
+
+    If `period is not None` and boundary conditions apply, residuals have the correct "local"
+    behavior, i.e., points to the left have a negative residual and points to the right have a
+    positive residual. This leads to a discontinuity a distance `period / 2` from any reference
+    point. The discontinuity is immaterial for even kernel functions.
 
     Args:
         x: Coordinates with shape `(..., p)`, where `...` is the batch shape and `p` is the number
@@ -23,16 +28,62 @@ def evaluate_squared_distance(x: ArrayOrTensor, y: ArrayOrTensor = None,
     # shape).
     if y is None:
         x, y = x[..., :, None, :], x[..., None, :, :]
-    residuals: ArrayOrTensor = x - y
-    if period is not None:
-        residuals = residuals % period
-        residuals = dispatch.minimum(residuals, period - residuals)
+    if period is None:
+        return x - y
+    residuals = (x % period) - (y % period)
+    residuals = dispatch.where(residuals > period / 2, residuals - period, residuals)
+    residuals = dispatch.where(residuals < - period / 2, residuals + period, residuals)
+    return residuals
+
+
+def evaluate_squared_distance(x: ArrayOrTensor, y: ArrayOrTensor = None,
+                              period: ArrayOrTensor = None) -> ArrayOrTensor:
+    """
+    Evaluate the squared distance between points respecting periodic boundary conditions.
+
+    Args:
+        x: Coordinates with shape `(..., p)`, where `...` is the batch shape and `p` is the number
+            of dimensions of the embedding space.
+        y: Coordinates with shape `(..., p)` which must be broadcastable to `x`. If not given, the
+            distance between the Cartesian product of `x` will be evaluated.
+        period: Period of circular boundary conditions.
+
+    Returns:
+        dist2: Squared distance between `x` and `y`.
+    """
+    residuals = evaluate_residuals(x, y, period)
     return (residuals * residuals).sum(axis=-1)
 
 
-class ExpQuadKernel:
+class Kernel:
     """
+    Base class for covariance kernels.
+    """
+    def __init__(self, epsilon: float = 0, period: ArrayOrTensor = None):
+        self.epsilon = epsilon
+        self.period = period
+
+    def __call__(self, x: ArrayOrTensor, y: ArrayOrTensor = None) -> ArrayOrTensor:
+        cov = self._evaluate(x, y)
+        if self.epsilon:
+            return cov + self.epsilon * dispatch[cov].eye(cov.shape[-1])
+        return cov
+
+    def _evaluate(self, x: ArrayOrTensor, y: ArrayOrTensor = None) -> ArrayOrTensor:
+        raise NotImplementedError
+
+    @property
+    def is_periodic(self):
+        return self.period is not None
+
+
+class ExpQuadKernel(Kernel):
+    r"""
     Exponentiated quadratic kernel.
+
+    .. math::
+
+        \text{cov}\left(x, y\right) = \alpha^2 \exp\left(-\frac{\left(x-y\right)^2}{2\rho^2}\right)
 
     Args:
         alpha: Scale of the covariance.
@@ -42,14 +93,10 @@ class ExpQuadKernel:
     """
     def __init__(self, alpha: float, rho: float, epsilon: float = 0, period: ArrayOrTensor = None) \
             -> None:
+        super().__init__(epsilon, period)
         self.alpha = alpha
         self.rho = rho
-        self.epsilon = epsilon
-        self.period = period
 
-    def __call__(self, x: ArrayOrTensor, y: ArrayOrTensor = None) -> ArrayOrTensor:
+    def _evaluate(self, x: ArrayOrTensor, y: ArrayOrTensor = None) -> ArrayOrTensor:
         exponent = - evaluate_squared_distance(x, y, self.period) / (2 * self.rho ** 2)
-        cov = self.alpha * self.alpha * dispatch.exp(exponent)
-        if self.epsilon:
-            return cov + self.epsilon * dispatch[x].eye(cov.shape[-1])
-        return cov
+        return self.alpha * self.alpha * dispatch.exp(exponent)
