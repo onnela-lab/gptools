055af34d5f378b526b16f0169f26bc07f3b1d157
Till Hoffmann <tillahoffmann@gmail.com>
2022-09-21 16:44:59 -0400

    Fix parameterization names and coverage.

--- gptools-stan/tests/test_profile.py
+++ gptools-stan/tests/test_profile.py
@@ -11,14 +11,15 @@ def _run_main(tmp_path: pathlib.Path, parameterization: str, noise_scale: float 
     """
     kwargs = {"iter_sampling": 2, "num_nodes": 7, "num_parents": 2} | kwargs
     path = tmp_path / "result.pkl"
-    __main__([parameterization, str(noise_scale), str(path), *dict2args(**kwargs)])
+    __main__([parameterization, str(noise_scale), str(path), "--show_diagnostics",
+              *dict2args(**kwargs)])
     with path.open("rb") as fp:
         return pickle.load(fp), kwargs
 
 
 @pytest.mark.parametrize("parameterization", [
-    "graph_centered", "graph_non_centered", "fourier_centered", "fourier_non_centered", "centered",
-    "non_centered"])
+    "graph_centered", "graph_non_centered", "fourier_centered", "fourier_non_centered",
+    "standard_centered", "standard_non_centered"])
 def test_profile(parameterization: str, tmp_path: pathlib.Path) -> None:
     result, kwargs = _run_main(tmp_path, parameterization)
 
@@ -27,12 +28,12 @@ def test_profile(parameterization: str, tmp_path: pathlib.Path) -> None:
 
 
 def test_profile_timeout(tmp_path: pathlib.Path) -> None:
-    result, kwargs = _run_main(tmp_path, "centered", iter_sampling=100_000, timeout=0.7)
+    result, kwargs = _run_main(tmp_path, "standard_centered", iter_sampling=100_000, timeout=0.7)
 
     assert result["fits"][0] is None
     assert abs(result["durations"] - kwargs["timeout"]) < 0.1
 
 
 def test_profile_max_chains(tmp_path: pathlib.Path) -> None:
-    result, kwargs = _run_main(tmp_path, "centered", max_chains=7)
+    result, kwargs = _run_main(tmp_path, "standard_centered", max_chains=7)
     assert len(result["fits"]) == kwargs["max_chains"]
