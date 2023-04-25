6875219780e9db1cdb7e9a7edbc2930bc81164ee
Till Hoffmann <tillahoffmann@gmail.com>
2023-04-25 18:53:22 -0400

    Add minimal reproducible tree density example.

--- stan/tests/test_examples.py
+++ stan/tests/test_examples.py
@@ -2,3 +2,28 @@ from gptools.util.testing import parameterized_notebook_tests
 
 
 test_example = parameterized_notebook_tests(__file__, "../docs")
+
+
+def test_minimal_trees() -> None:
+    """
+    Minimal reproducible example to include in the publication.
+    """
+    from gptools.stan import compile_model
+    import numpy as np
+
+    # Load tree frequency matrix, define padding, and prepare data dictionary.
+    frequency = np.loadtxt("data/tachve.csv", delimiter=",", dtype=int)
+    num_rows, num_cols = frequency.shape
+    padding = 10
+    data = {
+        "num_rows": num_rows,
+        "num_rows_padded": num_rows + padding,
+        "num_cols": num_cols,
+        "num_cols_padded": num_cols + padding,
+        "frequency": frequency,
+    }
+
+    # Compile model and fit it.
+    model = compile_model(stan_file="stan/docs/trees/trees.stan")
+    fit = model.sample(data, iter_sampling=10, iter_warmup=10, chains=1, seed=0)
+    print(fit.diagnose())
