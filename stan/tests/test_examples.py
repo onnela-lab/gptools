6e6c11b596db1eb5d342836643e33d12de909be9
Till Hoffmann <tillahoffmann@gmail.com>
2023-04-26 12:04:47 -0400

    Add training mask to minimal example.

--- stan/tests/test_examples.py
+++ stan/tests/test_examples.py
@@ -11,16 +11,17 @@ def test_minimal_trees() -> None:
     from gptools.stan import compile_model
     import numpy as np
 
-    # Load tree frequency matrix, define padding, and prepare data dictionary.
+    # Load tree frequency matrix, define padding, and apply training mask.
     frequency = np.loadtxt("data/tachve.csv", delimiter=",", dtype=int)
     num_rows, num_cols = frequency.shape
     padding = 10
+    train_mask = np.random.binomial(1, 0.8, frequency.shape)
     data = {
         "num_rows": num_rows,
         "num_rows_padded": num_rows + padding,
         "num_cols": num_cols,
         "num_cols_padded": num_cols + padding,
-        "frequency": frequency,
+        "frequency": np.where(train_mask, frequency, -1),
     }
 
     # Compile model and fit it.
@@ -35,10 +36,14 @@ def test_minimal_tube() -> None:
     """
     from gptools.stan import compile_model
     import json
+    import numpy as np
 
-    # Load station locations, edges, passenger numbers, and prepare data dictionary.
+    # Load station locations, edges, passenger numbers, and apply training mask.
     with open("data/tube-stan.json") as fp:
         data = json.load(fp)
+    train_mask = np.random.binomial(1, 0.8, data["num_stations"])
+    data["passengers"] = np.where(train_mask, data["passengers"], -1)
+
     data.update({
         "include_degree_effect": 1,
         "include_zone_effect": 1,
