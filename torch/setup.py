927c3a8f2a5822b59ba482fd912e4535aa0b9aff
Till Hoffmann <tillahoffmann@gmail.com>
2023-04-26 10:20:33 -0400

    Add python version lower bound to `setup.py`.

--- torch/setup.py
+++ torch/setup.py
@@ -9,6 +9,7 @@ setup(
     long_description_content_type="text/x-rst",
     packages=find_namespace_packages(),
     version="0.1.0",
+    python_requires=">=3.8",
     install_requires=[
         "gptools-util",
         "numpy",
